2010375456b36bb9bc4415386dc6cdd4188d8f48
Roy Mickos <roy.m.mickos@gmail.com>
2014-08-04 18:39:41 +0300

    explorating the main view - using sanitize to display trusted html

--- pkanban/static/pkanban/js/app.js
+++ pkanban/static/pkanban/js/app.js
@@ -1,5 +1,5 @@
 
-var pkanbanApi = angular.module('pkanban.api',["ngResource"]);
+var pkanbanApi = angular.module('pkanban.api',["ngResource","ngSanitize"]);
 
 pkanbanApi.factory('Wip', ['$resource', function($resource) {
   return $resource('/pk/wip', {}, {
@@ -9,14 +9,35 @@ pkanbanApi.factory('Wip', ['$resource', function($resource) {
 
 pkanbanApi.factory('Task', ['$resource', function($resource){
   return $resource('/pk/task/:id', {}, {
-    query: {method: 'GET', params: {}, isArray:false}
+    query: {method: 'GET', params: {}, isArray:false},
+    get: {method: 'GET', params: {id: '@id'}}
+  });
+}]);
+
+pkanbanApi.factory('Valuestream', ['$resource', function($resource) {
+  return $resource('/pk/valuestream/:streamname', {}, {
+    query: {method: 'GET', params: {}, isArray:true},
+    set: {method: 'GET', params: {streamname: '@streamname'}}
   });
 }]);
 
 var pkanbanApp = angular.module('pkanbanApp',["ui.bootstrap", "pkanban.api"]);
 
-pkanbanApp.controller('wipController', ['$scope', 'Wip', function($scope, Wip) {
-  $scope.wip = Wip.query();
+
+pkanbanApp.controller('wipController', ['$scope', '$sce', 'Wip', 'Valuestream',
+   function($scope, $sce, Wip, Valuestream) {
+     $scope.wip = Wip.query();
+     $scope.valuestreams = Valuestream.query();
+     $scope.getPhases = function(stream) {
+       var retval, streams = $scope.valuestreams.length;
+       for (i=0; i < streams; i++){
+         if ($scope.valuestreams[i].streamname === stream) {
+             retval = $scope.valuestreams[i].phases;
+             break;
+           }
+         }
+       return(retval);
+     };
 }]);
 
 pkanbanApp.controller('taskController', ['$scope', 'Task', function($scope, Task) {
